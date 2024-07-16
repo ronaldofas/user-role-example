49564e2dd57a371c6d0fa17f21ce29e2136ccfff
Ronaldo Silva <ronaldofas@gmail.com>
2024-07-16 00:24:13 -0300

    Refatoração de idioma.

--- Program.cs
+++ Program.cs
@@ -34,6 +34,9 @@ await DbInitializer.InitializeAsync(app.Services);
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
+} else 
+{
     app.UseExceptionHandler("/Home/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
